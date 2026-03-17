74f1508c0de5b062c7a344df49e5dff6979a7f22
hecarde2 <hectordico@hotmail.com>
2026-03-17 15:40:14 -0500

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -1,49 +1,51 @@
-#Programa de inventario
+inventario = []
+option = 0
 
-#Empezamos dandole la bienvenida al usuario al programa 
+while option != 4:
 
-print("bienvenido al sistema de inventario")
+    print("MENÚ DE INVENTARIO")
+    print("1 AGREGAR PRODUCTO")
+    print("2 MOSTRAR INVENTARIO")
+    print("3 CALCULAR ESTADISTICAS")
+    print("4 SALIR DEL SISTEMA")
 
-#Continuamos pidiendole los datos del producto al usuario(nombre del producto, precio y cantidad)
+    option = int(input("INGRESE UNA OPCIÓN: "))
 
+    if option == 1:
+        nombre = input("Nombre del producto: ")
+        precio = float(input("Precio: "))
+        cantidad = int(input("Cantidad: "))
 
-nombre=input("nombre del producto : ")
+        producto = {
+            "nombre": nombre,
+            "precio": precio,
+            "cantidad": cantidad
+        }
 
+        inventario.append(producto)
+        print("PRODUCTO AGREGADO CON EXITO.")
 
-#Usamos el while true en esta posicion para que en caso de que el usuario ingrese un valor no valido para el precio o la cantidad,
-#Se le pedira que ingrese un valor valido sin tener que volver a ingresar el nombre del producto.
+    elif option == 2:
+        if len(inventario) == 0:
+            print("NO HAY PRODUCTOS.")
+        else:
+            print("INVENTARIO")
+            for producto in inventario:
+                print(producto)
 
+    elif option == 3:
+        total_unidades = 0
+        valor_total = 0
 
-while True:
-    precio=float(input("Ingrese el precio del producto: $ "))
-    cantidad=int(input("Ingrese la cantidad del producto:  "))
+        for producto in inventario:
+            total_unidades += producto["cantidad"]
+            valor_total += producto["precio"] * producto["cantidad"]
 
-    #Colocamos condiciones que queremos que se cumplan, en este caso que el precio y la cantidad sean numeros positivos, 
-    #Si no se cumplen se le dara un mensaje de error al usuario y se le pedira que ingrese un valor valido.
+        print("Total de unidades:", total_unidades)
+        print("Valor total del inventario: $", valor_total)
 
-    if precio < 0:
-        print("Error: El precio y la cantidad deben ser números positivos.ejemplo(1, 2, 3...)")
-        print("Por favor, ingrese un precio válido.")
-    elif cantidad < 0:
-        print("Error: El precio y la cantidad deben ser números positivos.(ejemplo: 1, 2, 3...)")
-        print("Por favor, ingrese una cantidad válida.")
-        break
-
-    #Si se cumplen las condiciones Y el numero ingrsado es positivo, se le mostrara el total del inventario y los datos del producto ingresados por el usuario.
-
+    elif option == 4:
+        print("HASTA PRONTO")
 
     else:
-        costo_total=precio*cantidad
-        print("El total del inventario es: $", costo_total)
-                
-
-        print(f"nombre del producto: {nombre}")
-        print(f"precio del producto: $ {precio}")
-        print(f"cantidad del producto: {cantidad}")
-        print(f"costo total: $ {costo_total}")
-        break
-
-
-#Este programa es un sistema de inventario simple que permite al usuario ingresar el nombre, precio 
-#y cantidad de un producto, y luego calcula el costo total del inventario.
-#El programa también incluye validaciones para asegurarse de que el precio y la cantidad sean números positivos.
+        print("VUELVE A INTENTAR")
